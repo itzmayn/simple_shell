0fb72282cd58f387175d12f0b8d0d14d41d1cf9d
comspromo <93977501+comspromo@users.noreply.github.com>
2023-05-22 09:59:17 +0000

    add

--- shell.c
+++ shell.c
@@ -931,3 +931,14 @@ char *confirm_loc(char *command)
     free(path_array);
     return (NULL);
 }
+
+/**
+ * add - Adds two integers together
+ * @a: The first integer to add
+ * @b: The second integer to add
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
